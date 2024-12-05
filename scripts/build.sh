63a2f7a8c917b6fe8034fa2a3b0dd39ace0f8805
Chris Frazier <chrisfrazier0@users.noreply.github.com>
2024-12-04 23:54:18 -0500

    chore: added build script

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")/../"
+
+banner="/*
+ * Wisp v${npm_package_version}
+ * ${npm_package_homepage}
+ *
+ * Copyright (c) $(date +%Y) Chris Frazier
+ * Licensed under the ${npm_package_license} license
+ */"
+
+mkdir -p dist
+minify src/wisp.css > dist/wisp.css.mini
+minify src/wisp.js > dist/wisp.js.mini
+
+for file in dist/*.mini; do
+  echo "$banner" | cat - "$file" > "${file%.mini}"
+  rm -f "$file"
+done
